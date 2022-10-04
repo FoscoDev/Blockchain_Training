4add0fc9a12497be81fac830175422d6ba276fa0
JohnsonMuriuki <89764778+JohnsonMuriuki@users.noreply.github.com>
2022-10-04 13:42:44 +0300

    Create account_operations_transaction.py

---
+++ account_operations_transaction.py
@@ -0,0 +1,47 @@
+#Implementing a class accounts
+
+
+class BlcAccount: 
+    def __init__(self):
+     self.balance = 0
+     self.id = 0
+     private_key = BlcAccount.privateKey
+     public_key = BlcAccount.address
+
+    def genAccount():
+        pass
+    def addKeyPairToBlcAccount():
+        pass
+    def updateBalance():
+        pass
+    def getBalance():
+        pass
+    def printBalance():
+
+        pass
+    def createOperation():
+        pass
+
+#Implementing a class Operations
+class Operation:
+    def __init__(self):
+        self.sender = 0
+        self.receiver = 0
+        self.amount = 0
+        self.signature = 0
+
+    def createOperation():
+        pass
+    def verifyOperation():
+        pass
+#Implementing a class Transactions
+class Transaction:
+    def __init__(self):
+        self.id = 0
+        self.nonce = 0
+        self.operations = ['', '']
+
+
+    def createTransaction():
+        pass
+
